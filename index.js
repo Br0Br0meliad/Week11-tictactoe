0c15c3b6ce2945fc539f19ab338dc4bfc91b3f66
Br0Br0meliad <101954615+Br0Br0meliad@users.noreply.github.com>
2022-08-16 16:33:33 -0600

    set up board

---
+++ index.js
@@ -0,0 +1,14 @@
+const playerX = <i class="bi bi-x-lg"></i>
+
+const playerO = <i class="bi bi-circle"></i>
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 6],
+    [0, 4, 8],
+    [2, 4, 6],
+];
