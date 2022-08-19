f482311fa515abd338c8d46eb66327e37780721b
Br0Br0meliad <101954615+Br0Br0meliad@users.noreply.github.com>
2022-08-18 18:52:17 -0600

    continued

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const playerX = <i class="bi bi-x-lg"></i>
+const playerX = <i class="bi bi-x-lg"></i>;
 
-const playerO = <i class="bi bi-circle"></i>
+const playerO = <i class="bi bi-circle"></i>;
+
+const boardElement = document.getElementById('board');
+
+const turnDisplay = document.getElementById('gameStatus');
 
 const winningCombos = [
     [0, 1, 2],
@@ -12,3 +16,68 @@ const winningCombos = [
     [0, 4, 8],
     [2, 4, 6],
 ];
+
+
+const win = () => `Player ${currentPlayer} has won!`;
+
+const draw = () => `Game ended in a draw!`;
+
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+gameStatus.innerHTML = currentPlayerTurn();
+
+let currentPlayer = playerX;
+
+newGame();
+
+btnRestart.addEventListener('click', newGame);
+
+function newGame() {
+    currentPlayer = playerX;
+    squareElements.array.forEach(element => {
+        square.classList.remove(playerX)
+        square.classList.remove(playerO)
+    });
+
+
+}
+
+
+function cellClick (event) {
+    const square = event.target;
+    const currentPlayer = "X";
+    placePiece(square, currentPlayer)
+    if (checkWin(currentPlayer)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        takeTurns();
+    }
+};
+
+function placePiece(square, currentPlayer) {
+    square.classList.add(currentPlayer)
+}
+
+function takeTurns() {
+	currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    gameStatus.innerHTML = currentPlayerTurn();
+}
+
+
+
+
+newGame() {
+    currentPlayer = "X";
+    boardState = {}
+}
+
+
+
+
+
+
+
+
+function getWinner();
