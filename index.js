c80cacaf7ab52107fbdbd1e504b7df37abdd4eb7
Br0Br0meliad <101954615+Br0Br0meliad@users.noreply.github.com>
2022-08-21 20:45:41 -0600

    restructure

--- index.js
+++ index.js
@@ -1,83 +1,39 @@
-const playerX = <i class="bi bi-x-lg"></i>;
 
-const playerO = <i class="bi bi-circle"></i>;
 
-const boardElement = document.getElementById('board');
-
-const turnDisplay = document.getElementById('gameStatus');
-
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 6],
-    [0, 4, 8],
-    [2, 4, 6],
+const WINNING_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
 ];
 
-
 const win = () => `Player ${currentPlayer} has won!`;
-
-const draw = () => `Game ended in a draw!`;
-
+const draw = () => `It's a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
 
 gameStatus.innerHTML = currentPlayerTurn();
 
-let currentPlayer = playerX;
-
-newGame();
-
-btnRestart.addEventListener('click', newGame);
-
-function newGame() {
-    currentPlayer = playerX;
-    squareElements.array.forEach(element => {
-        square.classList.remove(playerX)
-        square.classList.remove(playerO)
-    });
 
 
-}
+
+startGame()
+
+resetButton.addEventListener('click', startGame)
 
 
-function cellClick (event) {
-    const square = event.target;
-    const currentPlayer = "X";
-    placePiece(square, currentPlayer)
-    if (checkWin(currentPlayer)) {
-        endGame(false);
-    } else if (isDraw()) {
-        endGame(true);
-    } else {
-        takeTurns();
-    }
+function startGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.square')
+               .forEach(square => square.innerHTML = "");
+    square.removeEventListener('click', squareClick);
+    square.addEventListener('click', squareClick, {once: true});
 };
 
-function placePiece(square, currentPlayer) {
-    square.classList.add(currentPlayer)
-}
-
-function takeTurns() {
-	currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    gameStatus.innerHTML = currentPlayerTurn();
-}
-
-
-
-
-newGame() {
-    currentPlayer = "X";
-    boardState = {}
-}
-
-
-
-
-
-
-
-
-function getWinner();
